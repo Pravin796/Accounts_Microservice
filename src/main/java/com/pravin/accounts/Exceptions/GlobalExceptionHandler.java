0a6947bbde008e29be478ff8b90377cfb818fc2b
Pravin796 <pravinswain232@gmail.com>
2026-07-08 23:23:13 +0530

    handeled the global exception handeleing logic and also added validation to all the requestbody and params api

--- src/main/java/com/pravin/accounts/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/pravin/accounts/Exceptions/GlobalExceptionHandler.java
@@ -1,29 +1,56 @@
 package com.pravin.accounts.Exceptions;
 
 import com.pravin.accounts.dtos.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler{
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponseDto> handelResourceNotFoundException(ResourceNotFoundException exception,
-                                                                                 WebRequest webRequest){
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ){
+        Map<String, String> validationErrors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String validationMessage = error.getDefaultMessage();
+            validationErrors.put(fieldName, validationMessage);
+        });
+
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handelGlobalException(Exception exception,
+                                                                  WebRequest webRequest){
     ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
             webRequest.getDescription(false),
-            HttpStatus.NOT_FOUND,
+            HttpStatus.INTERNAL_SERVER_ERROR,
             exception.getMessage(),
             LocalDateTime.now()
     );
 
-    return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
+    return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
     @ExceptionHandler(CustomerAlreadyExitsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExitsException exception,
                                                                                  WebRequest webRequest){
--- src/main/java/com/pravin/accounts/controllers/AccountsControllers.java
+++ src/main/java/com/pravin/accounts/controllers/AccountsControllers.java
@@ -4,21 +4,26 @@ import com.pravin.accounts.constants.AccountsConstants;
 import com.pravin.accounts.dtos.CustomerDto;
 import com.pravin.accounts.dtos.ResponseDto;
 import com.pravin.accounts.services.IAccountsService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @RequiredArgsConstructor
+@Validated
 public class AccountsControllers {
 
     private final IAccountsService iAccountsService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
 
         iAccountsService.createAccount(customerDto);
 
@@ -28,14 +33,16 @@ public class AccountsControllers {
     }
 
     @GetMapping("/fetch")
-    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) {
+    public ResponseEntity<CustomerDto> fetchAccountDetails(
+            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+            @RequestParam String mobileNumber) {
        CustomerDto customerDto =  iAccountsService.fetchAccount(mobileNumber);
 
        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = iAccountsService.updateAccount(customerDto);
         if(isUpdated) {
             return ResponseEntity
@@ -49,7 +56,9 @@ public class AccountsControllers {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam String mobileNumber) {
+    public ResponseEntity<ResponseDto> deleteAccountDetails(
+            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+            @RequestParam String mobileNumber) {
         boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
         if(isDeleted) {
             return ResponseEntity
